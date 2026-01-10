ce7401d63f286207e0d2021d3143b0f515eccaf1
Chase McDonald <chasecmcdonald@gmail.com>
2026-01-10 12:15:59 -0800

    update to 0.1.1 without gym requirement

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ import setuptools
 
 setuptools.setup(
     name="interactive_gym",
-    version="0.1.0",
+    version="0.1.1",
     description="A platform for running interactive experiments in the browser with standard simulation environments.",
     author="Chase McDonald",
     author_email="chasecmcdonald@gmail.com",
     packages=setuptools.find_packages(),
     install_requires=[
-        "gymnasium==1.0.0",
         "numpy",
     ],
     extras_require={
